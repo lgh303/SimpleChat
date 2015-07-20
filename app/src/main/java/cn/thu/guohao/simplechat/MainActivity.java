0ee50fba14543599fcfa55ddb3130dfef4f373dc
lgh <lcliguohao@163.com>
2015-07-21 00:36:01 +0800

    fragments framework

--- app/src/main/java/cn/thu/guohao/simplechat/MainActivity.java
+++ app/src/main/java/cn/thu/guohao/simplechat/MainActivity.java
@@ -1,25 +1,60 @@
 package cn.thu.guohao.simplechat;
 
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewConfiguration;
 
-import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity
+        implements ChatsFragment.OnFragmentInteractionListener{
+
+    public ViewPager mViewPager;
+    private FragmentPagerAdapter mPagerAdapter;
+    private List<Fragment> mTabs = new ArrayList<>();
+    private String[] mTitles = new String[] {
+            "Chats",
+            "Contacts",
+            "Discover",
+            "Me",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        setOverflowButtonAlways();
-        ActionBar bar = getSupportActionBar();
-        if (bar != null)
-            bar.setDisplayShowHomeEnabled(false);
+        initView();
+        initData();
+        mViewPager.setAdapter(mPagerAdapter);
+    }
+
+    private void initView() {
+        mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+    }
+
+    private void initData() {
+        for (String title : mTitles) {
+            ChatsFragment frag = ChatsFragment.newInstance(title);
+            mTabs.add(frag);
+        }
+        mPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                return mTabs.get(i);
+            }
+
+            @Override
+            public int getCount() {
+                return mTabs.size();
+            }
+        };
     }
 
     @Override
@@ -44,16 +79,8 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void setOverflowButtonAlways() {
-        ViewConfiguration config = ViewConfiguration.get(this);
-        try {
-            Field menuKey = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
-            menuKey.setAccessible(true);
-            menuKey.setBoolean(config, false);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
